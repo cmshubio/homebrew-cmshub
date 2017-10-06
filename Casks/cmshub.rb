42e49fbb9920becf80967b998e80ef8f94ebee62
CmsHub <hello@cmshub.io>
2017-10-06 16:52:56 +0200

    Add Cask

---
+++ Casks/cmshub.rb
@@ -0,0 +1,9 @@
+cask 'cmshub' do
+  version '0.1.0'
+
+  url "https://github.com/cmshubio/homebrew-cmshub/raw/master/cmshub"
+  name 'CmsHub'
+  homepage 'http://cmshub.io'
+
+  binary "cmshub"
+end
