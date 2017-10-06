dc63506ec1065099ef53f1d696f38eae97f1de5e
CmsHub <hello@cmshub.io>
2017-10-06 16:55:36 +0200

    Add checksum

--- Casks/cmshub.rb
+++ Casks/cmshub.rb
@@ -1,6 +1,8 @@
 cask 'cmshub' do
   version '0.1.0'
 
+  sha256 '0c0811527f5d858bf50d1f8bcaa0ac1dd113035e27cfffccdadbe045580d208d'
+
   url "https://github.com/cmshubio/homebrew-cmshub/raw/master/cmshub"
   name 'CmsHub'
   homepage 'http://cmshub.io'
