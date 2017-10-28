f2a312f41ddacddee487ab5792aaf22bec615b52
CmsHub <hello@cmshub.io>
2017-10-28 18:29:04 +0200

    Update cmshub cli

--- Casks/cmshub.rb
+++ Casks/cmshub.rb
@@ -1,7 +1,7 @@
 cask 'cmshub' do
   version '0.1.0'
 
-  sha256 '4d2c0818e83550fdbb5e2d0a9150e5ee58db3f9a1679183f52708e06d81ea5d4'
+  sha256 '27ff4548bf4cd8e9399dd44678e679d51c3631e19d7d2413e439e86bf5a26d8d'
 
   url "https://github.com/cmshubio/homebrew-cmshub/raw/master/cmshub"
   name 'CmsHub'
