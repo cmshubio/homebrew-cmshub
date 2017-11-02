08d6ebeb15c299ef6dec17c864b3754671095d01
CmsHub <hello@cmshub.io>
2017-11-02 13:47:55 +0100

    Fix mistake in sha256

--- Casks/cmshub.rb
+++ Casks/cmshub.rb
@@ -1,7 +1,7 @@
 cask 'cmshub' do
   version '0.1.0-rc1'
 
-  sha256 '90db5746fba5e25c788de614b720a4e43bbed5f219a44fe375c972043f584f79  homebrew-cmshub/cmshub'
+  sha256 '90db5746fba5e25c788de614b720a4e43bbed5f219a44fe375c972043f584f79'
 
   url "https://github.com/cmshubio/homebrew-cmshub/raw/master/cmshub"
   name 'CmsHub'
