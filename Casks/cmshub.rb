ec6125847346c9ced2b50975d22dabf8c65fbf25
CmsHub <hello@cmshub.io>
2017-11-17 18:07:06 +0100

    Release v0.1.0-rc8

--- Casks/cmshub.rb
+++ Casks/cmshub.rb
@@ -1,7 +1,7 @@
 cask 'cmshub' do
-  version '0.1.0-rc7'
+  version '0.1.0-rc8'
 
-  sha256 'b8f9384847cb391fd6404ba7861f62d0af00536ab4d84f19427743073b9d297b'
+  sha256 'a50bc0e13d70d961adb84024ee7bb8a589601ff40767f3b008830657d3dd0eee'
 
   url "https://github.com/cmshubio/homebrew-cmshub/raw/master/cmshub"
   name 'CmsHub'
